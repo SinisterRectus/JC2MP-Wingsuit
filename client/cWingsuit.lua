ea867ac0e64fde3100d6809add11f046bc516fb1
SinisterRectus <SinisterRectus@users.noreply.github.com>
2015-09-10 00:10:57 -0400

    Update cWingsuit.lua
    Nearly total re-write of the script to include a custom grapple, rendered wings, smooth camera transitions, and barrel rolls.

--- client/cWingsuit.lua
+++ client/cWingsuit.lua
@@ -1,28 +1,42 @@
 class 'Wingsuit'
 
 function Wingsuit:__init()
+
+	self.superman = false -- Enables superman physics (disables custom grapple)
+	self.grapple = true -- Enables custom grapple while gliding
+	self.barrel = true -- Enables barrel rolls
 	
 	self.default_speed = 51 -- 51 m/s default
-	self.max_speed = 300 -- 300 m/s default
-	self.min_speed = 1 -- 1 m/s default
-	self.sink_speed = 7 -- 7 ms default (for realism mode)
+	self.default_vertical_speed = -7 -- -7 ms default
+	
+	self.max_speed = 300 -- 300 m/s default, for superman mode
+	self.min_speed = 1 -- 1 m/s default, for superman mode
+	
+	self.tether_length = 150 -- meters
+	self.yaw_gain = 1.5
+	
 	self.speed = self.default_speed
+	self.vertical_speed = self.default_vertical_speed
 	
-	self.realism = false
-	
-	self.actions = { -- Actions to block while flying
-		Action.LookUp,
-		Action.LookDown,
-		Action.LookLeft,
-		Action.LookRight
+	self.blacklist = {
+		actions = { -- Actions to block while wingsuit is active
+			[Action.LookUp] = true,
+			[Action.LookDown] = true,
+			[Action.LookLeft] = true,
+			[Action.LookRight] = true
+		},
+		animations = { -- Disallow activation during these base states
+			[AnimationState.SDead] = true,
+			[AnimationState.SUnfoldParachuteHorizontal] = true,
+			[AnimationState.SUnfoldParachuteVertical] = true,
+			[AnimationState.SPullOpenParachuteVertical] = true
+		}
 	}
 	
-	self.blacklist = { -- Disallow activating during these base states
-		[AnimationState.SDead] = true,
-		[AnimationState.SUnfoldParachuteHorizontal] = true,
-		[AnimationState.SUnfoldParachuteVertical] = true,
-		[AnimationState.SPullOpenParachuteVertical] = true
+	self.timers = {
+		grapple = Timer()
 	}
+	self.subs = {}
 	
 	Events:Subscribe("KeyUp", self, self.Activate)
 	Events:Subscribe("ModulesLoad", self, self.AddHelp)
@@ -32,40 +46,45 @@ end
 
 function Wingsuit:Activate(args)
 
-	if args.key == VirtualKey.Shift and not self.velocity_sub and LocalPlayer:GetState() == PlayerState.OnFoot then
+	if args.key == VirtualKey.Shift and not self.subs.camera and LocalPlayer:GetState() == PlayerState.OnFoot then
 	
 		local bs = LocalPlayer:GetBaseState()
-		if self.blacklist[bs] then return end
+		if self.blacklist.animations[bs] then return end
 
-		if not self.timer or self.timer:GetMilliseconds() > 500 then
+		if not self.timers.activate or self.timers.activate:GetMilliseconds() > 500 then
 
-			self.timer = Timer()
+			self.timers.activate = Timer()
 
-		elseif self.timer:GetMilliseconds() < 500 and not self.delay then
+		elseif self.timers.activate:GetMilliseconds() < 500 and not self.subs.delay then
 
-			self.timer = nil		
+			self.timers.activate = nil
 			
-			if (not self.realism and (bs == AnimationState.SSkydive or bs == AnimationState.SParachute)) or (self.realism and bs == AnimationState.SSkydive) then
+			if (self.superman and (bs == AnimationState.SSkydive or bs == AnimationState.SParachute)) or (not self.superman and bs == AnimationState.SSkydive) then
 				
+				self.timers.camera_start = Timer()
 				self.speed = self.default_speed
 				LocalPlayer:SetBaseState(AnimationState.SSkydive)
-				self.velocity_sub = Events:Subscribe("Render", self, self.Velocity)
-				self.camera_sub = Events:Subscribe("CalcView", self, self.Camera)
-				self.input_sub = Events:Subscribe("InputPoll", self, self.InputBlock)
+				self.subs.wings = Events:Subscribe("GameRender", self, self.DrawWings)
+				self.subs.velocity = Events:Subscribe("Render", self, self.SetVelocity)
+				self.subs.camera = Events:Subscribe("CalcView", self, self.Camera)
+				self.subs.glide = Events:Subscribe("InputPoll", self, self.Glide)
+				self.subs.input = Events:Subscribe("LocalPlayerInput", self, self.Input)
 				
-			elseif not self.realism then
+			elseif self.superman then
 		
 				local timer = Timer()
-				self.camera_sub = Events:Subscribe("CalcView", self, self.Camera)
-				self.delay = Events:Subscribe("PreTick", self, function(self)
+				self.timers.camera_start = Timer()
+				self.subs.camera = Events:Subscribe("CalcView", self, self.Camera)
+				self.subs.input = Events:Subscribe("LocalPlayerInput", self, self.Input)
+				self.subs.wings = Events:Subscribe("GameRender", self, self.DrawWings)
+				self.subs.delay = Events:Subscribe("PreTick", function()
 					LocalPlayer:SetLinearVelocity(LocalPlayer:GetAngle() * Vector3(0, 5, -5))
 					if timer:GetMilliseconds() > 1000 then
-						Events:Unsubscribe(self.delay)
-						self.delay = nil
+						Events:Unsubscribe(self.subs.delay)
+						self.subs.delay = nil
 						self.speed = self.default_speed
 						LocalPlayer:SetBaseState(AnimationState.SSkydive)
-						self.velocity_sub = Events:Subscribe("Render", self, self.Velocity)
-						self.input_sub = Events:Subscribe("InputPoll", self, self.InputBlock)
+						self.subs.velocity = Events:Subscribe("Render", self, self.SetVelocity)
 					end
 				end)
 
@@ -73,13 +92,15 @@ function Wingsuit:Activate(args)
 			
 		end
 
-	elseif args.key == VirtualKey.Control and self.velocity_sub then
+	elseif args.key == VirtualKey.Control and self.subs.camera then
 	
-		if not self.timer or self.timer:GetMilliseconds() > 500 then
-			self.timer = Timer()
-		elseif self.timer:GetMilliseconds() < 500 then
-			LocalPlayer:SetLinearVelocity(Vector3.Zero)
-			LocalPlayer:SetBaseState(AnimationState.SFall)
+		if not self.timers.activate or self.timers.activate:GetMilliseconds() > 500 then
+			self.timers.activate = Timer()
+		elseif self.timers.activate:GetMilliseconds() < 500 then
+			local ray = Physics:Raycast(LocalPlayer:GetPosition(), Vector3(0, -1, -1), 0, 50)
+			if ray.distance < 30 then
+				LocalPlayer:SetBaseState(AnimationState.SFall)
+			end
 			self:Abort()
 		end
 
@@ -87,20 +108,56 @@ function Wingsuit:Activate(args)
 
 end
 
+function Wingsuit:DrawWings()
 
+	self.dt = math.abs((Game:GetTime() + 12) % 24 - 12) / 12
 
-function Wingsuit:Velocity()
+	local bones = LocalPlayer:GetBones()
+	local color = LocalPlayer:GetColor()
+	
+	local r = math.lerp(0.1 * color.r, color.r, self.dt)
+	local g = math.lerp(0.1 * color.g, color.g, self.dt)
+	local b = math.lerp(0.1 * color.b, color.b, self.dt)
+	
+	color = Color(r, g, b)
+	
+	Render:FillTriangle(
+		bones.ragdoll_RightArm.position, 
+		bones.ragdoll_RightForeArm.position,
+		bones.ragdoll_RightUpLeg.position, 
+		color
+	)
+	
+	Render:FillTriangle(
+		bones.ragdoll_LeftArm.position, 
+		bones.ragdoll_LeftForeArm.position,
+		bones.ragdoll_LeftUpLeg.position, 
+		color
+	)
+	
+	Render:DrawLine(
+		bones.ragdoll_RightForeArm.position,
+		bones.ragdoll_RightUpLeg.position,
+		Color.Black
+	)
+	
+	Render:DrawLine(
+		bones.ragdoll_LeftForeArm.position,
+		bones.ragdoll_LeftUpLeg.position,
+		Color.Black
+	)
 
-	if LocalPlayer:GetBaseState() ~= AnimationState.SSkydive then
+end
+
+function Wingsuit:SetVelocity()
+	
+	local bs = LocalPlayer:GetBaseState()
+
+	if bs ~= AnimationState.SSkydive and bs ~= AnimationState.SSkydiveDash then
 		self:Abort()
 	end
 	
-	if self.realism then
-	
-		local speed = self.speed - math.sin(LocalPlayer:GetAngle().pitch) * 20
-		LocalPlayer:SetLinearVelocity(LocalPlayer:GetAngle() * Vector3(0, 0, -speed) + Vector3(0, -self.sink_speed, 0))		
-	
-	else
+	if self.superman then
 	
 		if Key:IsDown(VirtualKey.Shift) and self.speed < self.max_speed then
 			self.speed = self.speed + 1
@@ -111,79 +168,267 @@ function Wingsuit:Velocity()
 		local speed = self.speed - math.sin(LocalPlayer:GetAngle().pitch) * 20
 		LocalPlayer:SetLinearVelocity(LocalPlayer:GetAngle() * Vector3(0, 0, -speed))
 		
+	else
+	
+		local speed = self.speed - math.sin(LocalPlayer:GetAngle().pitch) * 20
+		LocalPlayer:SetLinearVelocity(LocalPlayer:GetAngle() * Vector3(0, 0, -speed) 
+			+ Vector3(0, self.vertical_speed, 0))		
+	
 	end
 	
-	local speed_string = string.format("%i km/h   %i m", LocalPlayer:GetLinearVelocity():Length() * 3.6, LocalPlayer:GetPosition().y - 200)
-	local position = Vector2(0.5 * Render.Width - 0.5 * Render:GetTextWidth(speed_string, TextSize.Large), Render.Height - Render:GetTextHeight(speed_string, TextSize.Large))
+	local speed = LocalPlayer:GetLinearVelocity():Length() * 3.6
+	local player_pos = LocalPlayer:GetPosition()
+	local altitude = player_pos.y - (math.max(200, Physics:GetTerrainHeight(player_pos)))
+	local hud_str = string.format("%i km/h   %i m", speed, altitude)
+	local screen_pos = Vector2(0.5 * Render.Width - 0.5 * Render:GetTextWidth(hud_str, TextSize.Large), Render.Height - Render:GetTextHeight(hud_str, TextSize.Large))
 	
-	Render:DrawText(position, speed_string, Color.White, TextSize.Large)
+	Render:DrawText(screen_pos + Vector2(1,1), hud_str, Color.Black, TextSize.Large)
+	Render:DrawText(screen_pos, hud_str, Color.White, TextSize.Large)
+	
+	if not self.barrel then return end
+	
+	if Input:GetValue(Action.MoveLeft) > 0 then
+		if not self.roll_left then
+			self.roll_left = true
+			if not self.timers.roll_left then
+				self.timers.roll_left = Timer()
+			elseif self.timers.roll_left:GetMilliseconds() < 500 then
+				if not self.subs.roll_left then
+					local timer = Timer()
+					LocalPlayer:SetBaseState(AnimationState.SSkydiveDash)
+					self.subs.roll_left = Events:Subscribe("PreTick", function(args)
+						if timer:GetMilliseconds() > 750 then
+							LocalPlayer:SetBaseState(AnimationState.SSkydive)
+							Events:Unsubscribe(self.subs.roll_left)
+							self.subs.roll_left = nil
+						end
+					end)
+				end
+				self.timers.roll_left = nil
+			else
+				self.timers.roll_left = nil
+			end
+		end
+	else
+		self.roll_left = nil
+	end
+	
+	if Input:GetValue(Action.MoveRight) > 0 then
+		if not self.roll_right then
+			self.roll_right = true
+			if not self.timers.roll_right then
+				self.timers.roll_right = Timer()
+			elseif self.timers.roll_right:GetMilliseconds() < 500 then
+				if not self.subs.roll_right then
+					local timer = Timer()
+					LocalPlayer:SetBaseState(AnimationState.SSkydiveDash)
+					self.subs.roll_right = Events:Subscribe("PreTick", function(args)
+						if timer:GetMilliseconds() > 750 then
+							LocalPlayer:SetBaseState(AnimationState.SSkydive)
+							Events:Unsubscribe(self.subs.roll_right)
+							self.subs.roll_right = nil
+						end
+					end)
+				end
+				self.timers.roll_right = nil
+			else
+				self.timers.roll_right = nil
+			end
+		end
+	else
+		self.roll_right = nil
+	end
 
 end
 
-function Wingsuit:InputBlock(args)
-
-	for _, action in ipairs(self.actions) do
-		Input:SetValue(action, 0)
-	end
+function Wingsuit:Glide(args)
 	
-	if self.realism and Input:GetValue(Action.MoveBackward) > 0 and LocalPlayer:GetAngle().pitch > 0 then
-		Input:SetValue(Action.MoveBackward, 0)
+	if self.superman then return end
+
+	if not self.hit then
+
+		if Input:GetValue(Action.MoveBackward) > 0 and LocalPlayer:GetAngle().pitch > 0 then
+			Input:SetValue(Action.MoveBackward, 0)
+		end
+	
+	else
+	
+		Input:SetValue(Action.MoveBackward, 1)
+		
+		if self.yaw < 0 then
+			Input:SetValue(Action.MoveLeft, -self.yaw_gain * self.yaw)
+		elseif self.yaw > 0 then
+			Input:SetValue(Action.MoveRight, self.yaw_gain * self.yaw)
+		end
+
 	end
 
 end
 
-function Wingsuit:Camera()
+function Wingsuit:Input(args)
 
-	Camera:SetPosition(LocalPlayer:GetPosition() + LocalPlayer:GetAngle() * Vector3(0, 2, 7))
-	Camera:SetAngle(Angle.Slerp(Camera:GetAngle(), LocalPlayer:GetAngle(), 0.9))
+	if Game:GetState() ~= GUIState.Game then return end
+	
+	if not self.superman and self.grapple and args.input == Action.FireGrapple then
+	
+		if self.subs.grapple or self.subs.roll_left or self.subs.roll_right or self.timers.grapple:GetMilliseconds() < 500 then return false end
+		
+		local angle = LocalPlayer:GetAngle()
+		
+		if angle.pitch < -0.2 * math.pi then return false end
+			
+		LocalPlayer:SetLeftArmState(399)
+		
+		self.timers.grapple = Timer()
+		local direction = Angle(angle.yaw, 0, 0) * Vector3(-angle.roll, -0.3, -1)
+		
+		self.effect = ClientEffect.Create(AssetLocation.Game, {
+			effect_id = 11,
+			position = LocalPlayer:GetPosition(),
+			angle = Angle()
+		})
+
+		self.subs.grapple = Events:Subscribe("GameRender", function(args)
+			
+			local dt = self.timers.grapple:GetMilliseconds()			
+			local bone_pos = LocalPlayer:GetBonePosition("ragdoll_LeftForeArm")
+			local distance = self.tether_length * dt / 500
+			local ray = Physics:Raycast(bone_pos, direction, 0, distance)
+			
+			local color = Color.Silver
+			local r = math.lerp(0.5 * color.r, color.r, self.dt)
+			local g = math.lerp(0.5 * color.g, color.g, self.dt)
+			local b = math.lerp(0.5 * color.b, color.b, self.dt)
+			
+			Render:DrawLine(bone_pos, self.hit or ray.position, Color(r, g, b, 192))
+
+			if not self.hit then
+
+				if ray.distance < 0.9 * distance and ray.position.y > 200 then
+					self.hit = ray.position
+					self.vertical_speed = -self.vertical_speed
+				end
+
+				if dt > 500 then self:EndGrapple() end
+
+			else
+			 
+				local yaw1 = math.atan2(bone_pos.x - self.hit.x, bone_pos.z - self.hit.z)
+				local yaw2 = LocalPlayer:GetAngle().yaw
+				self.yaw = (yaw2 - yaw1 + math.pi) % (2 * math.pi) - math.pi
+				
+				if dt > 1500 or math.abs(self.yaw) > 0.2 * math.pi then self:EndGrapple() end
+	
+			end
+			
+		end)
+	
+		return false
+	
+	end
+		
+	if self.blacklist.actions[args.input] then return false end
+
+end
+
+function Wingsuit:EndGrapple()
+
+	self.timers.grapple:Restart()
+	self.effect:Remove()
+	LocalPlayer:SetLeftArmState(384)
+	Events:Unsubscribe(self.subs.grapple)
+	self.subs.grapple = nil
+	self.hit = nil
+	self.yaw = 0
+	self.vertical_speed = self.default_vertical_speed
+	self.speed = self.default_speed
 
 end
 
 function Wingsuit:Abort()
 
-	Events:Unsubscribe(self.velocity_sub)
-	Events:Unsubscribe(self.camera_sub)
-	Events:Unsubscribe(self.input_sub)
-	self.velocity_sub = nil
-	self.camera_sub = nil
-	self.input_sub = nil
+	for k,v in pairs(self.subs) do
+		if k ~= "camera" then
+			Events:Unsubscribe(v)
+			self.subs[k] = nil
+		end
+	end
+	
+	self.timers.camera_stop = Timer()
+
+end
+
+function Wingsuit:Camera()
+
+	if self.timers.camera_start then
+	
+		local dt = self.timers.camera_start:GetMilliseconds()
+
+		Camera:SetPosition(math.lerp(Camera:GetPosition(), LocalPlayer:GetPosition() + LocalPlayer:GetAngle() * Vector3(0, 2, 7), dt / 1000))
+		Camera:SetAngle(Angle.Slerp(Camera:GetAngle(), LocalPlayer:GetAngle(), 0.9 * dt / 1000))
+
+		if dt >= 1000 then 
+			self.timers.camera_start = nil 
+		end
+		
+	elseif self.timers.camera_stop then
+	
+		local dt = self.timers.camera_stop:GetMilliseconds()
+
+		Camera:SetPosition(math.lerp(LocalPlayer:GetPosition() + LocalPlayer:GetAngle() * Vector3(0, 2, 7), Camera:GetPosition(), dt / 1000))
+		Camera:SetAngle(Angle.Slerp(Camera:GetAngle(), LocalPlayer:GetAngle(), 0.9 - 0.9 * dt / 1000))
+
+		if dt >= 1000 then 
+			self.timers.camera_stop = nil
+			Events:Unsubscribe(self.subs.camera)
+			self.subs.camera = nil
+		end	
+		
+	else
+	
+		Camera:SetPosition(LocalPlayer:GetPosition() + LocalPlayer:GetAngle() * Vector3(0, 2, 7))
+		Camera:SetAngle(Angle.Slerp(Camera:GetAngle(), LocalPlayer:GetAngle(), 0.9))
+		
+	end
 
 end
 
 function Wingsuit:AddHelp()
 
-	if self.realism then
+	local text
 	
-		Events:Fire("HelpAddItem",
-			{
-				name = "Wingsuit",
-				text = 
-					"The wingsuit allows you to fly around Panau unencumbered." .. 
-					"\n\nTo activate, double-tap Shift while sky-diving." ..
-					"\nTo de-activate, double-tap Ctrl."
-			})	
-
+	if self.superman then
+		text = 
+			"This wingsuit allows you to fly around Panau unencumbered." .. 
+			"\n\nTo activate, double-tap Shift. To de-activate, double-tap Ctrl." ..
+			"\nWhile flying, hold Shift to speed up or Ctrl to slow down."
 	else
-
-		Events:Fire("HelpAddItem",
-			{
-				name = "Wingsuit",
-				text = 
-					"The wingsuit allows you to fly around Panau unencumbered." .. 
-					"\n\nTo activate, double-tap Shift. To de-activate, double-tap Ctrl." ..
-					"\nWhile flying, hold Shift to speed up or Ctrl to slow down."
-			})
-			
+		if self.grapple then
+			text = 
+				"The wingsuit allows you to glide gently from the sky." .. 
+				"\n\nTo activate, double-tap Shift while sky-diving." ..
+				"\nTo de-activate, double-tap Ctrl." ..
+				"\nUse your grapple to propel yourself across land."	
+		else
+			text = 
+				"The wingsuit allows you to fly around Panau unencumbered." .. 
+				"\n\nTo activate, double-tap Shift while sky-diving." ..
+				"\nTo de-activate, double-tap Ctrl."
+		end			
 	end
+
+	Events:Fire("HelpAddItem", {
+		name = "Wingsuit",
+		text = text
+	})
 
 end
 
 function Wingsuit:RemoveHelp()
 
-    Events:Fire("HelpRemoveItem",
-        {
-            name = "Wingsuit"
-        })
+	Events:Fire("HelpRemoveItem", {
+		name = "Wingsuit"
+	})
 
 end
 
